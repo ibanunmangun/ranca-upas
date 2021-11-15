ea12d04a786355518dc12f13eea86938f52504c3
ibanunmangun <62372980+ibanunmangun@users.noreply.github.com>
2021-11-15 18:28:45 +0800

    Add files via upload

---
+++ data/PoIV2_3.js
@@ -0,0 +1,16 @@
+var json_PoIV2_3 = {
+"type": "FeatureCollection",
+"name": "PoIV2_3",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Nama": "Penangkaran rusa", "X": 107.39276017682852, "Y": -7.1372155550301821, "Info": "Point of Interest" }, "geometry": { "type": "Point", "coordinates": [ 107.392760176828517, -7.137215555030182 ] } },
+{ "type": "Feature", "properties": { "Nama": "Kebun Strawberry", "X": 107.39325461870878, "Y": -7.1386661026596228, "Info": "Point of Interest" }, "geometry": { "type": "Point", "coordinates": [ 107.393254618708781, -7.138666102659623 ] } },
+{ "type": "Feature", "properties": { "Nama": "Onsen", "X": 107.3947402, "Y": -7.1404536, "Info": "Point of Interest" }, "geometry": { "type": "Point", "coordinates": [ 107.3947402, -7.1404536 ] } },
+{ "type": "Feature", "properties": { "Nama": "Kebun Bunga Rawa Abadi", "X": 107.3922398, "Y": -7.1359113, "Info": "Point of Interest" }, "geometry": { "type": "Point", "coordinates": [ 107.3922398, -7.1359113 ] } },
+{ "type": "Feature", "properties": { "Nama": "Healing Forest", "X": 107.39295249865796, "Y": -7.1350168276402126, "Info": "Point of Interest" }, "geometry": { "type": "Point", "coordinates": [ 107.392952498657962, -7.135016827640213 ] } },
+{ "type": "Feature", "properties": { "Nama": "Musholla Almardiyah", "X": 107.3924385, "Y": -7.138631199, "Info": "Public Facilities" }, "geometry": { "type": "Point", "coordinates": [ 107.3924385, -7.138631199 ] } },
+{ "type": "Feature", "properties": { "Nama": "Polsek Ciwidey", "X": 107.4650726, "Y": -7.102808711, "Info": "Public Facilities" }, "geometry": { "type": "Point", "coordinates": [ 107.4650726, -7.102808711 ] } },
+{ "type": "Feature", "properties": { "Nama": "Pertamina", "X": 107.4276708, "Y": -7.122340191, "Info": "Public Facilities" }, "geometry": { "type": "Point", "coordinates": [ 107.4276708, -7.122340191 ] } },
+{ "type": "Feature", "properties": { "Nama": "Tempat Parkir Ranca Upas", "X": 107.393119, "Y": -7.137877, "Info": "Public Facilities" }, "geometry": { "type": "Point", "coordinates": [ 107.393119, -7.137877 ] } }
+]
+}
